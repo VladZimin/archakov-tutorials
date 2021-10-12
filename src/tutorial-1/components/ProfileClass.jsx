735a247c76a9acf21ee145ec501d2899ab59585d
VladZimin <vlad.zimin1@gmail.com>
2021-10-12 17:45:00 +0600

    Сделал урок №1

---
+++ src/tutorial-1/components/ProfileClass.jsx
@@ -0,0 +1,13 @@
+import {Component} from "react"
+import style from './Profile.module.css'
+
+class ProfileClass extends Component {
+    render() {
+        return <div className={style.profileBlock}>
+            <h3 style={{fontWeight: "normal"}}> Привет, <b>{this.props.name.slice(0, 4)}!</b></h3>
+            <p> Дата регистрации: {this.props.registredAt}</p>
+        </div>
+    }
+}
+
+export default ProfileClass

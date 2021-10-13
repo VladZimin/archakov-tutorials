c28f310e33fecbe7421051f5a870c1cf3eb592b2
VladZimin <vlad.zimin1@gmail.com>
2021-10-13 19:22:41 +0600

    Добавил проверку на наличие пропсов

--- src/tutorial-1/components/ProfileFunc.jsx
+++ src/tutorial-1/components/ProfileFunc.jsx
@@ -1,10 +1,10 @@
 import style from './Profile.module.css'
 
-let ProfileClass = props => {
+let ProfileFunc = props => {
     return <div className={style.profileBlock}>
-        <h3 style={{fontWeight: 'normal'}}>Привет, <b>{props.name.slice(0, 4)}!</b></h3>
-        <p> Дата регистрации: 22 июня 2021</p>
+        <h3 style={{fontWeight: 'normal'}}>Привет, <b>{props.name ? props.name.slice(0, 4) : 'Гость'}!</b></h3>
+        <p> Дата регистрации: {props.registredAt}</p>
     </div>
 }
 
-export default ProfileClass
+export default ProfileFunc

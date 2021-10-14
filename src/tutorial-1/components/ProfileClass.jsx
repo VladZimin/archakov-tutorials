24b118e06fe9e3ae3eb6624604070ac47f15dccb
VladZimin <vlad.zimin1@gmail.com>
2021-10-14 15:07:31 +0600

    Сделал дз1

--- src/tutorial-1/components/ProfileClass.jsx
+++ src/tutorial-1/components/ProfileClass.jsx
@@ -4,7 +4,8 @@ import style from './Profile.module.css'
 class ProfileClass extends Component {
     render() {
         return <div className={style.profileBlock}>
-            <h3 style={{fontWeight: "normal"}}> Привет, <b>{this.props.name ? this.props.name.slice(0, 4) : 'Гость'}!</b></h3>
+            <h3 style={{fontWeight: "normal"}}>
+                Привет, <b>{this.props.name ? this.props.name.split(' ', 1) : 'Гость'}!</b></h3>
             <p> Дата регистрации: {this.props.registredAt}</p>
         </div>
     }
--- src/tutorial-1/components/ProfileFunc.jsx
+++ src/tutorial-1/components/ProfileFunc.jsx
@@ -2,7 +2,8 @@ import style from './Profile.module.css'
 
 let ProfileFunc = props => {
     return <div className={style.profileBlock}>
-        <h3 style={{fontWeight: 'normal'}}>Привет, <b>{props.name ? props.name.slice(0, 4) : 'Гость'}!</b></h3>
+        <h3 style={{fontWeight: 'normal'}}>
+            Привет, <b>{props.name ? props.name.split(' ', 1) : 'Гость'}!</b></h3>
         <p> Дата регистрации: {props.registredAt}</p>
     </div>
 }

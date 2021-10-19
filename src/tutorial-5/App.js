71723c86da3891e5bdae3e936c43679cdfa9bb0b
VladZimin <vlad.zimin1@gmail.com>
2021-10-19 19:27:36 +0600

    Сделал ДЗ №5

---
+++ src/tutorial-5/App.js
@@ -0,0 +1,12 @@
+import style from './App.module.css'
+import FeedBackBlock from './components/FeedBackBlock'
+
+const App = () => {
+  return (
+    <div className={style.wrapper}>
+      <FeedBackBlock />
+    </div>
+  )
+}
+
+export default App

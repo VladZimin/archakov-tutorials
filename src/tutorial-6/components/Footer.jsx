4150e83a1f231d9b0a4a5969063f8266db9d26dd
VladZimin <vlad.zimin1@gmail.com>
2021-10-22 14:34:45 +0600

    Внес правки и создал компоненту Route

---
+++ src/tutorial-6/components/Footer.jsx
@@ -0,0 +1,11 @@
+import { Navbar } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <Navbar bg="light" style={{ paddingLeft: 20 }}>
+      <Navbar.Brand href="#home">My site (c) 2021</Navbar.Brand>
+    </Navbar>
+  )
+}
+
+export default Footer
---
+++ src/tutorial-6/components/Header.jsx
@@ -0,0 +1,30 @@
+import { Nav } from 'react-bootstrap'
+
+const Header = () => {
+  return (
+    <header>
+      <h2>
+        <a href="/">React Blog</a>
+      </h2>
+      <Nav variant="pills" defaultActiveKey="/">
+        <Nav.Item>
+          <Nav.Link eventKey="/home" to="/">
+            Главная
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="/home" to="/about">
+            Обо мне
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="/home" to="/profile">
+            Профиль
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </header>
+  )
+}
+
+export default Header

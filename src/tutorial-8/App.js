2aa1f1f1c056677d6c1ca43787af4ccaed5d10c5
VladZimin <vlad.zimin1@gmail.com>
2021-10-27 19:52:57 +0500

    Сделал ДЗ №8

---
+++ src/tutorial-8/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const App = () => {
+  const [users, setUsers] = React.useState([])
+  const getUsers = async () => {
+    try {
+      let response = await fetch(
+        'https://61795c43aa7f3400174049f4.mockapi.io/users'
+      )
+      let users = await response.json()
+      setUsers(users)
+    } catch (error) {
+      alert(error)
+    }
+  }
+  return (
+    <div>
+      <ul>
+        {users.map((obj) => (
+          <li key={obj.id}>{obj.name}</li>
+        ))}
+      </ul>
+      <button onClick={getUsers}>Сгенерировать пользователей</button>
+    </div>
+  )
+}
+export default App
